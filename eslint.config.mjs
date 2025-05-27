c8ec14ddba4b83d6731bc3346c42ad6f4f052528
freedomcly <freedomcly@hotmail.com>
2025-05-27 14:10:51 +0800

    blog-version1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,12 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+    rules: {
+      quotes: [2, 'single']
+    }
+  })
 ];
 
 export default eslintConfig;
